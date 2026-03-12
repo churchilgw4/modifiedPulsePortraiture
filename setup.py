a7f4b1e921558c920bf31bbef9605913a86f400c
AvinashKumarPaladi <88394644+AvinashKumarPaladi@users.noreply.github.com>
2026-03-12 12:11:14 +0530

    Add metafile_maker_cc_b35.py to scripts list

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='modifiedPulsePortraiture',
       author_email='avinashkumarpaladi@gmail.com',
       url='https://github.com/AvinashKumarPaladi/modifiedPulsePortraiture',
       py_modules=[ 'mpplib', 'mpptoas', 'mpptoaslib', 'mpp_telescope_codes', 'mpplib_b35', 'mpptoas_b35', 'mpptoaslib_b35', 'mpptoaslib_MLAN', 'mpptoaslib_b35_MLAN', 'mpp_gaussian_test'],
-      scripts=['mpptoas.py', 'mpptoas_b35.py']
+      scripts=['mpptoas.py', 'mpptoas_b35.py', 'metafile_maker_cc_b35.py']
      )
